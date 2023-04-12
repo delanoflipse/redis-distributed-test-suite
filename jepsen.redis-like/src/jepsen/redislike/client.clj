dc14f716dd6b9e6830d57571dc261d59f71baa60
Delano Flipse <delanoflipse@hotmail.com>
2023-04-12 16:47:59 +0200

    add basic error catcher

--- jepsen.redis-like/src/jepsen/redislike/client.clj
+++ jepsen.redis-like/src/jepsen/redislike/client.clj
@@ -12,6 +12,50 @@
             [jepsen.redislike.jedis :as jedis]
             [jepsen.redislike.database :as db-def]))
 
+(defmacro with-exceptions
+  "Takes an operation, an idempotent :f set, and a body; evaluates body,
+  converting known exceptions to failed ops."
+  [op idempotent & body]
+  `(let [crash# (if (~idempotent (:f ~op)) :fail :info)]
+     (try+ ~@body
+           (catch [:prefix :err] e#
+             (condp re-find (.getMessage (:throwable ~'&throw-context))
+               ; These two would ordinarily be our fault, but are actually
+               ; caused by follower proxies mangling connection state.
+               #"ERR DISCARD without MULTI"
+               (assoc ~op :type crash#, :error :discard-without-multi)
+
+               #"ERR MULTI calls can not be nested"
+               (assoc ~op :type crash#, :error :nested-multi)
+
+               (throw+)))
+           (catch [:prefix :moved] e#
+             (assoc ~op :type :fail, :error :moved))
+
+           (catch [:prefix :nocluster] e#
+             (assoc ~op :type :fail, :error :nocluster))
+
+           (catch [:prefix :clusterdown] e#
+             (assoc ~op :type :fail, :error :clusterdown))
+
+           (catch [:prefix :notleader] e#
+             (assoc ~op :type :fail, :error :notleader))
+
+           (catch [:prefix :timeout] e#
+             (assoc ~op :type crash# :error :timeout))
+
+           (catch java.io.EOFException e#
+             (assoc ~op :type crash#, :error :eof))
+
+           (catch java.net.ConnectException e#
+             (assoc ~op :type :fail, :error :connection-refused))
+
+           (catch java.net.SocketException e#
+             (assoc ~op :type crash#, :error [:socket (.getMessage e#)]))
+
+           (catch java.net.SocketTimeoutException e#
+             (assoc ~op :type crash#, :error :socket-timeout)))))
+
 (defrecord RedisClient [conn]
   ;; TODO: error handling
   jepsen.client/Client
@@ -24,10 +68,11 @@
   (setup! [_ test])
 
   (invoke! [this test op]
-    (case (:f op)
-      :read (assoc op :type :ok, :value (mapv parse-long (.lrange conn "foo" 0 -1)))
-      :write (do
-               (.rpush conn "foo" (into-array String [(str (:value op))]))
-               (assoc op :type :ok))))
+    (with-exceptions op #{}
+      (case (:f op)
+        :read (assoc op :type :ok, :value (mapv parse-long (.lrange conn "foo" 0 -1)))
+        :write (do
+                 (.rpush conn "foo" (into-array String [(str (:value op))]))
+                 (assoc op :type :ok)))))
 
   (teardown! [_ test]))
