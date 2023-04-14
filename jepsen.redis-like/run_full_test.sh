76c39c0ca877b94f53424dac65a3c78825ac0e97
Delano Flipse <delanoflipse@hotmail.com>
2023-04-14 12:27:42 +0200

    hotfix wrong cli argument full test

--- jepsen.redis-like/run_full_test.sh
+++ jepsen.redis-like/run_full_test.sh
@@ -1,13 +1,13 @@
-lein run test -c 3 --replicas 0 --rate 10 --test-run 2
-lein run test -c 6 --replicas 0 --rate 10 --test-run 2
-lein run test -c 9 --replicas 0 --rate 10 --test-run 2
-lein run test -c 6 --replicas 1 --rate 10 --test-run 2
-lein run test -c 9 --replicas 1 --rate 10 --test-run 2
-lein run test -c 9 --replicas 2 --rate 10 --test-run 2
+lein run test -c 3 --replicas 0 --rate 10 --test-count 2
+lein run test -c 6 --replicas 0 --rate 10 --test-count 2
+lein run test -c 9 --replicas 0 --rate 10 --test-count 2
+lein run test -c 6 --replicas 1 --rate 10 --test-count 2
+lein run test -c 9 --replicas 1 --rate 10 --test-count 2
+lein run test -c 9 --replicas 2 --rate 10 --test-count 2
 
-lein run test -c 3 --replicas 0 --rate 100 --test-run 2
-lein run test -c 6 --replicas 0 --rate 100 --test-run 2
-lein run test -c 9 --replicas 0 --rate 100 --test-run 2
-lein run test -c 6 --replicas 1 --rate 100 --test-run 2
-lein run test -c 9 --replicas 1 --rate 100 --test-run 2
-lein run test -c 9 --replicas 2 --rate 100 --test-run 2
+lein run test -c 3 --replicas 0 --rate 100 --test-count 2
+lein run test -c 6 --replicas 0 --rate 100 --test-count 2
+lein run test -c 9 --replicas 0 --rate 100 --test-count 2
+lein run test -c 6 --replicas 1 --rate 100 --test-count 2
+lein run test -c 9 --replicas 1 --rate 100 --test-count 2
+lein run test -c 9 --replicas 2 --rate 100 --test-count 2
