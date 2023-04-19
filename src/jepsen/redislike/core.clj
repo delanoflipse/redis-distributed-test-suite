b951cf9cc38b092ba3cf7ecff0713493db825d46
Delano Flipse <delanoflipse@hotmail.com>
2023-04-19 20:17:02 +0200

    doc: Cleanup and explain core functionality

--- src/jepsen/redislike/core.clj
+++ src/jepsen/redislike/core.clj
@@ -16,33 +16,37 @@
             [jepsen.os.debian :as debian]
             [jepsen.tests.cycle.append :as append]))
 
-
+;; Convert nth node to hostname. e.g. 1 -> n1
 (defn as-node-host [num] (str "n" (+ num 1)))
 
+;; Our test definition
 (defn redis-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts#]
-  (let [opts (merge opts# (db-def/db-opts! opts#))
+  (let [;; We inject some inferred arguments here, that way you can more easily switch databases.
+        opts (merge opts# (db-def/db-opts! opts#))
+        ;; Define our DB
         db (db-def/db)
+        ;; for automatic fault detection, we expect :db in opts
         opts     (assoc opts :db db)
+        ;; Elle append test auto client payload creation.
+        ;; This was quite hidden, luckily Redis Raft also used it.
         workload (append/test
-                  {; Exponentially distributed, so half of the time it's gonna
-                    ; be one key, 3/4 of ops will use one of 2 keys, 7/8 one of
-                    ; 3 keys, etc.
-                   :key-count          (:key-count opts)
-                   :min-txn-length     1
-                   :max-txn-length     (:max-txn-length opts 1)
+                  {:key-count          (:key-count opts) ;; If you know a way to make these keys actually evenly point to the same node (due to sharding), please let us know.
+                   :min-txn-length     1 ;; Writing nothing is useless.
+                   :max-txn-length     (:max-txn-length opts 1) ;; You can do more than one, but Redis Cluster has no transactions so yeah...
                    :max-writes-per-key (:max-writes-per-key opts 128)
-                   :consistency-models [:strict-serializable]})
+                   :consistency-models [:strict-serializable]}) ;; Change this for a more fair assesment.
+        ;; Our friendly neighbourhood nemesis, and its corresponding properties.
         nemesis (db-nemesis/nemesis opts)]
 
     (merge tests/noop-test
            opts
-
            {:name "redislike"
             :os   debian/os
             :db   db
+            ;; inject nodes as a list of n1 - nN. Bit easier than writing --nodes=n1,n2,etc...
             :nodes (into [] (map as-node-host (range (:node-count opts))))
             :client (client/->RedisClient nil)
             :generator       (->>
@@ -58,31 +62,27 @@
                                        :exceptions  (checker/unhandled-exceptions)
                                        :workload (:checker workload)
                                        :stats       (checker/stats)})})))
-
-(def nemeses
+;; CLI validation utilites
+(def possible-faults
   "Types of faults a nemesis can create."
   #{:fail-over :partition :packet :kill :pause :clock})
 
-(def special-nemeses
-  "A map of special nemesis names to collections of faults"
+(def special-faults
+  "A map of special faults names to collections of faults"
   {:none      []
    :standard  [:fail-over :partition :packet :kill :pause :clock]
    :all       [:fail-over :partition :packet :kill :pause :clock]})
 
-(defn parse-nemesis-spec
-  "Takes a comma-separated nemesis string and returns a collection of keyword
+(defn parse-fault-spec
+  "Takes a comma-separated faults string and returns a collection of keyword
   faults."
   [spec]
   (->> (str/split spec #",")
        (map keyword)
-       (mapcat #(get special-nemeses % [%]))
+       (mapcat #(get special-faults % [%]))
        (into [])))
 
-(defn as-keyword
-  "Convert to keyword"
-  [stringlike]
-  (keyword stringlike))
-
+;; CLI options
 (def cli-opts
   "Options for test runners."
   [["-c" "--node-count INT" "Amount of nodes"
@@ -105,17 +105,17 @@
     :default 7000]
 
    [nil "--faults FAULTS" "A comma-separated list of nemesis faults to enable"
-    :parse-fn parse-nemesis-spec
-    :validate [(partial every? (into nemeses (keys special-nemeses)))
-               (str "Faults must be one of " nemeses " or "
-                    (cli/one-of special-nemeses))]]
+    :parse-fn parse-fault-spec
+    :validate [(partial every? (into possible-faults (keys special-faults)))
+               (str "Faults must be one of " possible-faults " or "
+                    (cli/one-of special-faults))]]
 
    [nil "--replicas INT" "Replicas per primary"
     :parse-fn parse-long
     :default 1]
 
-   ["-d" "--database NAME" "redis or keydb"
-    :parse-fn as-keyword
+   ["-d" "--database NAME" "The database to test"
+    :parse-fn keyword
     :validate [(partial contains? db-def/db-defaults) (cli/one-of (keys db-def/db-defaults))]
     :default :redis]
 
@@ -128,6 +128,7 @@
     :parse-fn read-string
     :validate [#(and (number? %) (pos? %)) "must be a positive number"]]])
 
+;; Start here:
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
